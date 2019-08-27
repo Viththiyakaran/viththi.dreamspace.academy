88a82e9be7e47940c3ad041c57744a51a5959107
Viththiyakaran <battikaran6@gmail.com>
2019-08-27 18:25:49 +0530

    Update eduction details

--- education.php
+++ education.php
@@ -4,11 +4,8 @@
 			<div class="row justify-content-center" id="education">
 				<div class="col-lg-8 text-center">
 					<div class="main_title">
-						<h2>service offers </h2>
-						<p>
-							Is give may shall likeness made yielding spirit a itself togeth created 
-							after sea <br> is in beast beginning signs open god you're gathering ithe
-						</p>
+						<h2>Education </h2>
+						
 					</div>
 				</div>
 			</div>
@@ -16,29 +13,29 @@
 				<div class="col-lg-3 col-md-6">
 					<div class="feature_item">
 						<img src="img/services/s1.png" alt="">
-						<h4>Wp developing</h4>
-						<p>Creeping for female light years that lesser can't evening heaven isn't bearing tree</p>
+						<h4>CAA</h4>
+						<p >Certificate in Computer Application Assistant,Vocational Training Authority</p>
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6">
 					<div class="feature_item">
 						<img src="img/services/s2.png" alt="">
-						<h4>UI/ux design</h4>
-						<p>Creeping for female light years that lesser can't evening heaven isn't bearing tree</p>
+						<h4>ICTT</h4>
+						<p>Certificate in Information Commination Technology Technician,Vocational Training Authority.</p>
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6">
 					<div class="feature_item">
 						<img src="img/services/s3.png" alt="">
-						<h4>Web design</h4>
-						<p>Creeping for female light years that lesser can't evening heaven isn't bearing tree</p>
+						<h4>ICT DIPLOMA</h4><br>
+						<p>Completed Diploma course at National Vocational Training Institute, Vantharumoolai </p>
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6">
 					<div class="feature_item">
 						<img src="img/services/s4.png" alt="">
-						<h4>seo optimize</h4>
-						<p>Creeping for female light years that lesser can't evening heaven isn't bearing tree</p>
+						<h4>B.Tech in Software</h4>
+						<p>Following B.Tech in Software Technology at University of Vocational Technology</p>
 					</div>
 				</div>
 			</div>
