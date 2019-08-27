4642efade32b207ff5acc48471c5515edb650f17
Viththiyakaran <battikaran6@gmail.com>
2019-08-27 17:19:21 +0530

    Update expriences and links

--- experience.php
+++ experience.php
@@ -1,41 +1,50 @@
 	<!--================ Srart Brand Area =================-->
 	<section class="brand_area section_gap_bottom" id="experience">
 		<div class="container">
+		
+		<div class="row justify-content-center" >
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<h2> Experience </h2>			
+					</div>
+				</div>
+			</div>
+		
 			<div class="row justify-content-center">
 				<div class="col-lg-6">
 					<div class="row">
 						<div class="col-lg-4 col-md-4 col-sm-6">
 							<div class="single-brand-item d-table">
 								<div class="d-table-cell text-center">
-									<img src="img/brands/logo1.png" alt="">
+									<a href="https://wikiphp1.blogspot.com/"><img src="img/brands/logo1.png" alt="blogger"> </a>
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-6">
 							<div class="single-brand-item d-table">
 								<div class="d-table-cell text-center">
-									<img src="img/brands/logo2.png" alt="">
+									<a href="https://sylcsl.org/"><img src="img/brands/logo2.png" alt="Web Developer"></a>
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-6">
 							<div class="single-brand-item d-table">
 								<div class="d-table-cell text-center">
-									<img src="img/brands/logo3.png" alt="">
+									<a href="http://www.dreamspace.academy/pages/about/index.php#team"><img src="img/brands/logo3.png" alt="IT Trainer"></a>
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-6">
 							<div class="single-brand-item d-table">
 								<div class="d-table-cell text-center">
-									<img src="img/brands/logo4.png" alt="">
+									<a href="https://battimeals.com/"><img src="img/brands/logo4.png" alt="Battimeals"></a>
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-6">
 							<div class="single-brand-item d-table">
 								<div class="d-table-cell text-center">
-									<img src="img/brands/logo5.png" alt="">
+									<a href="https://www.youtube.com/channel/UCi2Pk7ZAEA5rDy1ymjMpPAQ"><img src="img/brands/logo5.png" alt="Batticaloa 360 YouTube Channel"></a>
 								</div>
 							</div>
 						</div>
@@ -46,33 +55,15 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-4 col-md-4 col-sm-6">
-							<div class="single-brand-item d-table">
-								<div class="d-table-cell text-center">
-									<img src="img/brands/logo7.png" alt="">
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-6">
-							<div class="single-brand-item d-table">
-								<div class="d-table-cell text-center">
-									<img src="img/brands/logo8.png" alt="">
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-sm-6">
-							<div class="single-brand-item d-table">
-								<div class="d-table-cell text-center">
-									<img src="img/brands/logo9.png" alt="">
-								</div>
-							</div>
-						</div>
+						
+						
+						
 					</div>
 				</div>
 				<div class="offset-lg-2 col-lg-4 col-md-6">
 					<div class="client-info">
 						<div class="d-flex mb-50">
-							<span class="lage">10</span>
+							<span class="lage">03</span>
 							<span class="smll">Years Experience Working</span>
 						</div>
 						<div class="call-now d-flex">
@@ -81,7 +72,7 @@
 							</div>
 							<div class="ml-15">
 								<p>call us now</p>
-								<h3>(+1)-800-555-6789</h3>
+								<h3>(+94) 75 41 14 487</h3>
 							</div>
 						</div>
 					</div>
